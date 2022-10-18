81db11be342e937cc6d905f2027743ac3cf7abf2
SebSCam <sebas9903@outlook.com>
2022-10-17 20:26:47 -0500

    terminado

--- client.py
+++ client.py
@@ -4,28 +4,28 @@ import threading
 tag = input("Enter a tag to Client: ")
 
 host = '127.0.0.1'
-port = 5102
+port = 5101
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
 
 
 def receive_messages():
-    message = ''
-    while message.lower().strip() != 'bye':
+    while True:
         message = client.recv(1024).decode('utf-8')
         if message == "@id":
             client.send(tag.encode("utf-8"))
         else:
-            print(message)
-
-    print("Server finished all connections")
-    client.close()
+            print(f"server says: {message}")
 
 def write_messages():
-    while True:
+    message = ""
+    while message.lower().strip() != 'bye':
         message = input('')
         client.send(message.encode('utf-8'))
+
+    client.close()
+    print("Client Disconnected")
 
 receive_thread = threading.Thread(target=receive_messages)
 receive_thread.start()
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
+from email import message
 import socket   
 import threading
 
 host = '127.0.0.1'
-port = 5102
+port = 5101
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -13,39 +14,40 @@ print(f"Server running on {host}:{port}")
 clients = []
 tags = []
 
-def broadcast(message):
-    for client in clients:
-        client.send(message.encode("utf-8"))
-
-def handle_messages(client):
+def show_messages(client):
     while True:
         index = clients.index(client)
         tag = tags[index]
         message = client.recv(1024).decode("utf-8")
+        if message.lower().strip() == 'bye':
+            clients.remove(client)
+            tags.remove(tag)    
+            print(f"{tag} is offline")
+            break
         print(f"{tag} says: {message}")
+
+def broadcast(message):
+    for client in clients:
+        client.send(message.encode("utf-8"))
 
 def write_messages():
     while True:
-        message = f"Broadcast from Server -> {input('')}"
+        message = input('')
         broadcast(message)
 
 def receive_connections():
     while True:
         client, address = server.accept()
-
         client.send("@id".encode("utf-8"))
         tag = client.recv(1024).decode('utf-8')
-
         clients.append(client)
         tags.append(tag)
 
         print(f"{tag} is connected with {str(address)}")
-
         client.send("Connected to server".encode("utf-8"))
 
 
-
-        thread = threading.Thread(target=handle_messages, args=(client,))
+        thread = threading.Thread(target=show_messages, args=(client,))
         thread.start()
 
         thread = threading.Thread(target=write_messages)
